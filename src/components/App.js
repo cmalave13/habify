b6318319d13e94e8cc19688bfd3540fc76ad24b6
Charlie Malave <malavecharles@gmail.com>
2021-06-15 18:10:42 -0400

    pre-commit to master

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import NavBar from "./NavBar.jsx";
 import GoalBox from "./GoalBox.jsx";
+
 //import { Switch, Route } from "react-router-dom";
 //import MyHabits from "./MyHabits/MyHabits.jsx";
 
