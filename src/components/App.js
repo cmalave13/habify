f7f288d3a0e1e96f1f35f74d0311965bf277b1da
Charlie Malave <malavecharles@gmail.com>
2021-06-15 20:34:50 -0400

    checkbox with data grid

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import NavBar from "./NavBar.jsx";
 import GoalBox from "./GoalBox.jsx";
+import GoalBox2 from "./GoalBox2.jsx";
 
 //import { Switch, Route } from "react-router-dom";
 //import MyHabits from "./MyHabits/MyHabits.jsx";
@@ -11,7 +12,7 @@ class App extends Component {
       <div>
         <NavBar></NavBar>
         <br />
-        <GoalBox></GoalBox>
+        <GoalBox2></GoalBox2>
       </div>
     );
   }
